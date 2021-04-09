d108e77eabb1e09df89bf6fb966b51ec0ee6c4d9
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-08 22:41:48 -0400

    pls work

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
+from .forms import Userform
 
 # Create your views here.
 
 def index(req):
     return render(req, 'index.html')
+
+def signup(req):
+    if req.method == 'POST':
+        form = Userform(req.POST or None)
+        if form.is_valid():
+            form.save()
+        else:
+            return HttpResponse('bro u dunce')
+    return render(req, 'signup.html')
